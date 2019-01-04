479e4f38d9e632cae43ede16762ff63cf55601cc
Branko Straub <brankostraub@cnchenarc.fareast.corp.microsoft.com>
2019-01-03 22:30:09 -0300

    Add samples py

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
+num1=12
+num2=10
+
+def sumar(el1, el2):
+    return el1+el2
+
+if num1 > num2:
+    print("num 1 re peola")
+else:
+   print("num 2 re peola")
+
+print(sumar(10,50))
+
 a=0
 
 for i in range(5):
-    a+=5
-    print(a)
+    a+=1 
+
+print(a)
